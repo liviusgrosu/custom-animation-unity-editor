d0345c56db6fc1c6151f6b82602b4759b4f4e656
liviusgrosu <livius.grosu@ryerson.ca>
2021-04-10 20:20:44 -0300

    Renamed cart class to mining claw

--- Assets/MiningClaw.cs
+++ Assets/MiningClaw.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cart : MonoBehaviour
+public class MiningClaw : MonoBehaviour
 {
     [Header("Rail")]
     public CurveCreator RailEditor;
