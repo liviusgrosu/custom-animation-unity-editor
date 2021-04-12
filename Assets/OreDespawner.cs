b945324d67aa4c48b44cab1c79849bc1d8083e63
liviusgrosu <livius.grosu@ryerson.ca>
2021-04-11 21:14:06 -0300

    Implemented a ore despawner zone

---
+++ Assets/OreDespawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreDespawner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col) {
+        if (col.tag == "Ore") {
+            Destroy(col.gameObject);
+        }
+    }
+}
--- Assets/OreDispenser.cs
+++ Assets/OreDispenser.cs
@@ -8,14 +8,18 @@ public class OreDispenser : MonoBehaviour
     public GameObject[] OreObjects;
     public int OreMaxAmount = 5;
 
+    private float _maxOreSpawnLife = 30f;
+
     public void DispenseOre(float secondsToWait) {
         StartCoroutine(SpawnOre(secondsToWait));
     }
 
     IEnumerator SpawnOre(float secondsToWait) {
         for(int oreIdx = 0; oreIdx < OreMaxAmount; oreIdx++){
-            Instantiate(OreObjects[Random.Range(0, OreObjects.Length)], OreSpawn.position, Quaternion.identity);
+            GameObject oreInstance = Instantiate(OreObjects[Random.Range(0, OreObjects.Length)], OreSpawn.position, Quaternion.identity);
             yield return new WaitForSeconds(secondsToWait / OreMaxAmount);
+            // Destroy in case they fall off infinitely 
+            Destroy(oreInstance, _maxOreSpawnLife);
         }
     }
 }
